d62033d74c2eb3438b7eebe5c001f77976e4bb59
unknown <omogetiashvili11@gmail.com>
2023-06-14 23:41:13 +0400

    jshomework1

---
+++ index.js
@@ -0,0 +1,42 @@
+let month  ="august";
+
+
+switch (month){
+  case month = "june":
+    console.log("Summer")
+     break;
+  case month = "july":
+    console.log("Summer")
+     break;
+  case month = "august":
+    console.log("Summer")
+     break;
+  case month = "sectember":
+    console.log("autumn")
+     break;
+  case month = "octomber":
+    console.log("Autumn")
+     break;
+  case month = "november":
+    console.log("Autumn")
+     break;
+  case month = "december":
+    console.log("winter")
+     break;
+  case month = "janurary":
+    console.log("winter")
+     break;
+  case month = "february":
+    console.log("winter")
+     break;
+  case month = "march":
+    console.log("spring")
+     break;
+  case month = "april":
+    console.log("spring")
+     break;
+  case month = "may":
+    console.log("Summer")
+     break;
+
+}
